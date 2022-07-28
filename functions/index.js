2979cd6d6cd22e1b4db64cd33cee9dc3a4599f25
Bridgette Lemus <bridgettelemus@Bridgettes-MacBook-Pro.local>
2022-07-28 15:48:14 -0400

    first firebase api

---
+++ functions/index.js
@@ -0,0 +1,33 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+// set up routes
+
+app.get("/test", (req, res) => {
+  res.send("wow, it actually worked.");
+});
+
+app.get("/another-one", (req, res) => {
+  res.send("This one works too!");
+});
+
+exports.api = functions.https.onRequest(app);
+
+exports.helloThere = functions.https.onRequest((req, res) => {
+  res.send("Hello there Firebase Cloud user!");
+});
+
+
+
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
